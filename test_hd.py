c3809848c8be259c821c85b46c2b0742dde3f846
Seledriac <tom.boumba7@gmail.com>
2020-05-27 12:23:46 +0200

    multiple prediction tests for hd_test

--- test_hd.py
+++ test_hd.py
@@ -51,62 +51,68 @@ print("\nTest on the validation data -> deserialized network accuracy : {0}%\n".
 print("Wow ! Your model can recognize 28x28px images of handwritten digits with an accuracy of {0}% !\n".format(accuracy))
 
 
-#Prediction test
+#Prediction tests
 
-#The user choses a number to predict
 re = True
 while re:
-    try:
-        chosen_nb = int(input("\nThere is an example for each digit in the custom_test_images folder. Enter the number you want the model to recognize based on theese custom test images : "))
-        assert chosen_nb >= 0 and chosen_nb <=9
-        re = False
-    except AssertionError:
-        print("\nError, the chosen number isn't a single digit.")
-    except ValueError:
-        print("\nError, you didn't enter a valid digit.")
+    
+    #The user choses a number to predict
+    re1 = True
+    while re1:
+        try:
+            chosen_nb = int(input("\nThere is an example for each digit in the custom_test_images folder. Enter the number you want the model to recognize based on theese custom test images : "))
+            assert chosen_nb >= 0 and chosen_nb <=9
+            re1 = False
+        except AssertionError:
+            print("\nError, the chosen number isn't a single digit.")
+        except ValueError:
+            print("\nError, you didn't enter a valid digit.")
 
-#The image filename is retrieved
-# img_filename = "custom_test_images/mnist_example.bmp"
-img_filename = "custom_test_images/test_image_"+str(chosen_nb)+".bmp"
+    #The image filename is retrieved
+    # img_filename = "custom_test_images/mnist_example.bmp"
+    img_filename = "custom_test_images/test_image_"+str(chosen_nb)+".bmp"
 
-#Predicting the image
-from PIL import Image
-import numpy as np
-test_image = Image.open(img_filename)
-arr = 1 - np.array(test_image).reshape(784,1) / 255. #Conversion from image to array : 256-RGB to greyscale inverted (1 is black, 0 is white)
-model_activations = dn.feedforward(arr)
-print("\nAccording to the IA, the plotted number is {0} !\n".format(np.argmax(model_activations)))
+    #Predicting the image
+    from PIL import Image
+    import numpy as np
+    test_image = Image.open(img_filename)
+    arr = 1 - np.array(test_image).reshape(784,1) / 255. #Conversion from image to array : 256-RGB to greyscale inverted (1 is black, 0 is white)
+    model_activations = dn.feedforward(arr)
+    print("\nAccording to the IA, the plotted number is {0} !\n".format(np.argmax(model_activations)))
 
-#Plotting the test_image, and the activations, in subplots (one plots the image, the other plots the model's activation)
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-test_image = mpimg.imread(img_filename)
-plt.figure(figsize = (11, 5))
-plt.title("Prediction results")
-plt.subplot(121)
-plt.title("custom image")
-plt.imshow(test_image)
-plt.subplot(122)
-plt.title("corresponding model activations")
-plt.xlabel("digit")
-plt.ylabel("activation")
-axes = plt.gca()
-axes.set_ylim([0, 1])
-plt.xticks(range(10))
-plt.yticks(np.array(range(11))/10)
-plt.plot(range(10), model_activations)
-def annot_max(x, y, ax=axes):
-    xmax = x[np.argmax(y)]
-    ymax = y.max()
-    text= "digit = {}, activation = {:.3f}".format(xmax,ymax)
-    if not ax:
-        ax=plt.gca()
-    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
-    arrowprops=dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=60")
-    kw = dict(xycoords='data',textcoords="axes fraction",
-              arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
-    ax.annotate(text, xy=(xmax, ymax), xytext=(xmax/10 - 0.1, ymax - 0.1), **kw)
-annot_max(range(10), model_activations)
-plt.show()
+    #Plotting the test_image, and the activations, in subplots (one plots the image, the other plots the model's activation)
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    test_image = mpimg.imread(img_filename)
+    plt.figure(figsize = (11, 5))
+    plt.title("Prediction results")
+    plt.subplot(121)
+    plt.title("custom image")
+    plt.imshow(test_image)
+    plt.subplot(122)
+    plt.title("corresponding model activations")
+    plt.xlabel("digit")
+    plt.ylabel("activation")
+    axes = plt.gca()
+    axes.set_ylim([0, 1])
+    plt.xticks(range(10))
+    plt.yticks(np.array(range(11))/10)
+    plt.plot(range(10), model_activations)
+    def annot_max(x, y, ax=axes):
+        xmax = x[np.argmax(y)]
+        ymax = y.max()
+        text= "digit = {}, activation = {:.3f}".format(xmax,ymax)
+        if not ax:
+            ax=plt.gca()
+        bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+        arrowprops=dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=60")
+        kw = dict(xycoords='data',textcoords="axes fraction",
+                arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
+        ax.annotate(text, xy=(xmax, ymax), xytext=(xmax/10 - 0.1, ymax - 0.1), **kw)
+    annot_max(range(10), model_activations)
+    plt.show()
+
+    #Ask for a new prediction
+    re = str(input("predict another custom digit ? (O/N) : ")).lower() == "o"
 
 
